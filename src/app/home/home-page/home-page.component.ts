a038b3db3178a543578ac1aaa32263f4ce06927b
huberberrio <huber.berrio@gmail.com>
2023-05-22 09:09:12 -0500

    Add reponsiveness to HomePage

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  isTablet$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Tablet)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  isWeb$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Web)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+    constructor(
+      private breakpointObserver: BreakpointObserver,
+    ) {}
 
 }
